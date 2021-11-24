3f60f07ac285dd6890625b741449908a3e2d2293
perik <perik@DESKTOP-ISGD1EV>
2021-11-24 11:09:28 +0500

    Добавьте файлы проекта.

---
+++ masslinq/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+
+namespace masslinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (File.Exists("qwe.txt"))
+            {
+                StreamWriter sw = new StreamWriter("qwe.txt");
+                Console.WriteLine("Введите колличество элементов массива: ");
+                try
+                {
+                    int k = Convert.ToInt32(Console.ReadLine()); 
+                    int[] a = new int[k];
+                for (int i = 0; i < k; i++)
+                {
+                    Console.Write("Введите числo: ", i);
+                    a[i] = Convert.ToInt32(Console.ReadLine());
+                }
+                IEnumerable<int> ev = from i in a
+                                      where i % 2 == 0
+                                      select i;
+                foreach (int i in ev)
+                {
+                    Console.WriteLine(i);
+                    sw.Write(i);
+                }
+                sw.Close();
+                }
+                catch { Console.WriteLine("Вы ввели не число"); }
+            }
+            else
+                Console.WriteLine("Файл не найден");      
+        }
+    }
+}
